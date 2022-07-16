8c33e2c0fe6d8a62122b2109bbf826d883b81d6b
Josué Casasola <jcasasola@musicbattles.mx>
2022-07-16 10:45:09 -0500

    core: angular files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'bikepolo-tournament-23118',
+    appId: '1:1076692081716:web:72407314f1c31fd8e5fe61',
+    storageBucket: 'bikepolo-tournament-23118.appspot.com',
+    locationId: 'us-west2',
+    apiKey: 'AIzaSyB9vFI6RC2SACUm7eCptzH34J-WMF9LTBY',
+    authDomain: 'bikepolo-tournament-23118.firebaseapp.com',
+    messagingSenderId: '1076692081716',
+    measurementId: 'G-EE1QJZ89V6',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'bikepolo-tournament-23118',
+    appId: '1:1076692081716:web:72407314f1c31fd8e5fe61',
+    storageBucket: 'bikepolo-tournament-23118.appspot.com',
+    locationId: 'us-west2',
+    apiKey: 'AIzaSyB9vFI6RC2SACUm7eCptzH34J-WMF9LTBY',
+    authDomain: 'bikepolo-tournament-23118.firebaseapp.com',
+    messagingSenderId: '1076692081716',
+    measurementId: 'G-EE1QJZ89V6',
+  },
+  production: false, 
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
